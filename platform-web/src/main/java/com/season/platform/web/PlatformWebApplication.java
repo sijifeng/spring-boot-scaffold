908983dc0c1876efb71c513d7bc4a8ff86cc2248
季迎春 <jiyc@kingnet.com>
2017-02-16 14:24:57 +0800

    push 脚手架

---
+++ platform-web/src/main/java/com/season/platform/web/PlatformWebApplication.java
@@ -0,0 +1,16 @@
+package com.season.platform.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jiyc on 2017/2/13.
+ */
+
+@SpringBootApplication
+public class PlatformWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformWebApplication.class, args);
+	}
+}
